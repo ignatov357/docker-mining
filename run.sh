66aba026893ef54a0fc22b54578b317e62add961
ignatov357 <kosnaum@mail.ru>
2018-05-02 15:57:22 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
-ccminer -a cryptonight -o stratum+tcp://bcn.pool.minergate.com:45550 -u ignatov357@mail.ru -p x
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:ethereum/ethereum
+sudo apt-get update
+sudo apt-get install cpp-ethereum
+ethminer -U -S eth.pool.minergate.com:45791 -u ignatov357@mail.ru
