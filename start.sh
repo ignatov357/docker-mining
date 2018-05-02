ed1823bbdc518e8bfea5e3a6580a5c70b5db2d1f
ignatov357 <kosnaum@mail.ru>
2018-05-02 21:03:36 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,26 @@
-apt-get update
-apt-get upgrade -y
-apt-get install -y wget
-wget https://github.com/ethereum-mining/ethminer/releases/download/v0.14.0rc8/ethminer-0.14.0rc8-Linux.tar.gz -O ethminer.tar.gz
-tar -xvzf ethminer.tar.gz
-cd bin
-#./ethminer -U -M
-./ethminer -U -S eth.pool.minergate.com:45791 -u ignatov357@mail.ru -v 0
+#apt-get update
+#apt-get upgrade -y
+#apt-get install -y wget
+#wget https://github.com/ethereum-mining/ethminer/releases/download/v0.14.0rc8/ethminer-0.14.0rc8-Linux.tar.gz -O ethminer.tar.gz
+#tar -xvzf ethminer.tar.gz
+#cd bin
+##./ethminer -U -M
+#./ethminer -U -S eth.pool.minergate.com:45791 -u ignatov357@mail.ru -v 0
+
+
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_7.5-18_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1404_7.5-18_amd64.deb
+sudo apt-get -y install software-properties-common
+sudo add-apt-repository -y ppa:ethereum/ethereum
+sudo apt-get update
+sudo apt-get install git cmake libcryptopp-dev libleveldb-dev libjsoncpp-dev libjson-rpc-cpp-dev libboost-all-dev libgmp-dev libreadline-dev libcurl4-gnutls-dev ocl-icd-libopencl1 opencl-headers mesa-common-dev libmicrohttpd-dev build-essential cuda -y
+git clone https://github.com/Genoil/cpp-ethereum/
+cd cpp-ethereum/
+mkdir build
+cd build
+cmake -DBUNDLE=cudaminer ..
+make -j8
+ls
+cd ethminer
+ls
+ethminer -U -M
