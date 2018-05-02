ad03a678af8cf34988d99ced6f48fe906a805f80
ignatov357 <kosnaum@mail.ru>
2018-05-02 17:28:25 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,6 @@ apt-get update
 apt-get install -y wget
 wget https://github.com/ethereum-mining/ethminer/releases/download/v0.14.0rc8/ethminer-0.14.0rc8-Linux.tar.gz -O ethminer.tar.gz
 tar -xvzf ethminer.tar.gz
+cd bin
 ethminer -U -M
 #ethminer -U -S eth.pool.minergate.com:45791 -u ignatov357@mail.ru
