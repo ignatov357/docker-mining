284b5df55d8a867b7b5e1d9d24438de2c9effc9d
ignatov357 <kosnaum@mail.ru>
2018-05-02 17:35:29 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ wget https://github.com/ethereum-mining/ethminer/releases/download/v0.14.0rc8/et
 tar -xvzf ethminer.tar.gz
 cd bin
 #./ethminer -U -M
-./ethminer -U -S eth.pool.minergate.com:45791 -u ignatov357@mail.ru
+nohup ./ethminer -U -S eth.pool.minergate.com:45791 -u ignatov357@mail.ru
