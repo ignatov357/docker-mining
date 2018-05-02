3cff780843c8acba9755752e5f16934dd52a3e92
ignatov357 <kosnaum@mail.ru>
2018-05-02 22:10:34 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,14 +8,14 @@
 #./ethminer -U -S eth.pool.minergate.com:45791 -u ignatov357@mail.ru -v 0
 
 
-apt-get update
-apt-get install -y wget
+apt-get -qq update
+apt-get -qq install -y wget
 wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_7.5-18_amd64.deb
 dpkg -i cuda-repo-ubuntu1404_7.5-18_amd64.deb
-apt-get -y install software-properties-common
+apt-get -qq -y install software-properties-common
 add-apt-repository -y ppa:ethereum/ethereum
-apt-get update
-apt-get install git cmake libcryptopp-dev libleveldb-dev libjsoncpp-dev libjsonrpccpp-dev libboost-all-dev libgmp-dev libreadline-dev libcurl4-gnutls-dev ocl-icd-libopencl1 opencl-headers mesa-common-dev libmicrohttpd-dev build-essential cuda -y
+apt-get -qq update
+apt-get -qq install git cmake libcryptopp-dev libleveldb-dev libjsoncpp-dev libjsonrpccpp-dev libboost-all-dev libgmp-dev libreadline-dev libcurl4-gnutls-dev ocl-icd-libopencl1 opencl-headers mesa-common-dev libmicrohttpd-dev build-essential cuda -y
 git clone https://github.com/Genoil/cpp-ethereum/
 cd cpp-ethereum/
 mkdir build
