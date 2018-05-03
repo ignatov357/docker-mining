e3c06729c8fba3b9fa36d2b4130d3024582192ec
ignatov357 <kosnaum@mail.ru>
2018-05-04 01:29:23 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@
 #cd bin
 chmod 777 ethminer
 ./ethminer -U -M
-./ethminer -U -S eth.pool.minergate.com:45791 -u ignatov357@mail.ru -v 0
+./ethminer -U -S eu1.ethpool.org:3333 -u 0xB64986e9f06534740fBc018bBCF1e8C204909180 -v 0
 
 
 #apt-get -qq update
