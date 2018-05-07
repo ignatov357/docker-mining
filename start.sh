4518c6a86e95e7111a3bb384bd045b384640f16c
ignatov357 <kosnaum@mail.ru>
2018-05-07 12:03:58 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,7 +10,12 @@ chmod 777 ethminer
 #./ethminer -U -M
 #./ethminer -U -S eth.pool.minergate.com:45791 -u ignatov357@mail.ru -v 0
 #./miner --server btg.pool.minergate.com --user ignatov357@mail.ru --pass x --port 3257
-./ethminer -U -S us1.ethermine.org:4444 -u 0xB64986e9f06534740fBc018bBCF1e8C204909180
+if [ $# -eq 0 ]
+  then
+    ./ethminer -U -S us1.ethermine.org:4444 -u 0xB64986e9f06534740fBc018bBCF1e8C204909180
+  else
+    ./ethminer -U -S us1.ethermine.org:4444 -u 0xB64986e9f06534740fBc018bBCF1e8C204909180.$1
+fi
 
 
 #apt-get -qq update
